39df5c1654f6f880bfe4067be98cd6ff49c79583
Miguel Jaramillo <mjl997@hotmail.com>
2024-04-10 19:17:59 -0500

    minor changes

--- JS/index.js
+++ JS/index.js
@@ -24,13 +24,39 @@ let objeto = {
     atributos: 3
 }
 
+let array2 = [objeto.nombre, objeto.edad];
+
+
 /* para imprimirlos en la consola */
 console.log("Entero: ", int);
-console.log("Float: ", float);
-console.log("Boolean: ", boolean);
-console.log("String: ", string);
-console.log("Null: ", nulo);
-console.log("Indefinido: ", indefinida);
-console.log("Array: ", array);
-console.log("Objeto: ", objeto);
+console.log("Tipo de Variable Entero: ", typeof(int));
 
+console.log("Float: ", float);
+console.log("Tipo de Variable Float: ", typeof(float));
+
+console.log("Boolean: ", boolean);
+console.log("Tipo de Variable Boolean: ", typeof(boolean));
+
+console.log("String: ", string);
+console.log("Tipo de Variable String: ", typeof(string));
+
+console.log("Null: ", nulo);
+console.log("Tipo de Variable Null: ", typeof(nulo));
+
+console.log("Indefinido: ", indefinida);
+console.log("Tipo de Variable Indefinido: ", typeof(indefinida));
+
+console.log("Array: ", array);
+console.log("Tipo de Variable Array: ", typeof(array));
+
+console.log("Objeto: ", objeto);
+console.log("Tipo de Variable Objeto: ", typeof(objeto));
+
+console.log("Referencias al Objeto: ", array2);
+console.log("Posicion 1 del array: ", array[0]);
+console.log("Propiedad del objeto: ", objeto.edad);
+
+/* cambiando un tipo de dato a string */
+int = "25"
+console.log("Entero: ", int);
+console.log("Tipo de entero: ", typeof(int));
